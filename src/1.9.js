e87f0a76ac5f25f44413058a71eb2d2809502060
Katriin <katriin.poolakese@khk.ee>
2021-01-26 22:30:37 +0200

    1.9

---
+++ src/1.9.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
+
+const Statistics = (props) => {
+  if (props == 0) {
+    return <p>No feedback given</p>;
+  }
+  return (
+    <p>
+      {props.text}: {props.value}
+      {props.symbol}
+    </p>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+
+  return (
+    <div>
+      <h1>feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h1>stats</h1>
+      {all === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <div>
+          <Statistics text="good" value={good} />
+          <Statistics text="neutral" value={neutral} />
+          <Statistics text="bad" value={bad} />
+          <Statistics text="all" value={all} />
+          <Statistics text="average" value={average} />
+          <Statistics text="positive" value={(100 / all) * good} symbol="%" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
